cd179b7896cfceab663147460f5aa4a2e79ca0ba
Valentin Rusev <val.rusev@gmail.com>
2025-02-26 12:19:01 +0100

    Hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac.d
@@ -0,0 +1,5 @@
+/Users/valentin/projects/the-rust-programming-language/hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac: src/main.rs
+
+/Users/valentin/projects/the-rust-programming-language/hello_cargo/target/debug/deps/hello_cargo-53e8430b693fb2ac.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/valentin/projects/the-rust-programming-language/hello_cargo/target/debug/hello_cargo: /Users/valentin/projects/the-rust-programming-language/hello_cargo/src/main.rs
