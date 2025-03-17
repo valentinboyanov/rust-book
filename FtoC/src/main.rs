a06fb1af59fd0c8f686e6c0159dd5f9d5f9faff9
Valentin Rusev <val.rusev@gmail.com>
2025-03-17 10:20:12 +0100

    Improvements

--- FtoC/src/main.rs
+++ FtoC/src/main.rs
@@ -1,41 +1,44 @@
 use std::io;
 
+// Improvements:
+// - handle negative numbers
+// - handle precision problems, eg: 123°F => 50.56°C, 50.56°C => 123.01°F
+// - add unit tests
+// - implement switch mode with second loop
+
 fn main() {
     println!("Convert temperatures between Fahrenheit and Celsius.");
 
-    println!("Please, select origin: F or C");
+    loop {
+        println!("Introduce temperature (eg. 123F or 65C):");
 
-    let mut origin = String::new();
+        let mut input = String::new();
 
-    io::stdin()
-        .read_line(&mut origin)
-        .expect("Failed to read line.");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line.");
 
-    let origin: char = origin
-        .trim()
-        .parse()
-        .expect("Failed to parse origin.");
+        if input.trim() == "exit" {
+            break;
+        }
 
-    println!("Please, introduce temperature:");
+        let mut temperature = input.trim().to_owned();
 
-    let mut temp_from = String::new();
+        let temp_unit = temperature.pop();
 
-    io::stdin()
-        .read_line(&mut temp_from)
-        .expect("Failed to read line.");
+        let temp_from: f64 = temperature
+            .trim()
+            .parse()
+            .expect("Failed to parse temperature.");
 
-    let temp_from: f64 = temp_from
-        .trim()
-        .parse()
-        .expect("Failder to parse temperature.");
-
-    if origin == 'F' {
-        let temp_to: f64 = (temp_from - 32.0) / (9.0/5.0);
-        println!("{temp_from}°F => {temp_to:.2}°C",);
-    } else if origin == 'C' {
-        let temp_to: f64 = temp_from * (9.0/5.0) + 32.0;
-        println!("{temp_from}°C => {temp_to:.2}°F",);
-    } else {
-        println!("Unknown origin.");
+        if  temp_unit == Some('F') {
+            let temp_to: f64 = (temp_from - 32.0) / (9.0/5.0);
+            println!("{temp_from}°F => {temp_to:.2}°C",);
+        } else if temp_unit == Some('C') {
+            let temp_to: f64 = temp_from * (9.0/5.0) + 32.0;
+            println!("{temp_from}°C => {temp_to:.2}°F",);
+        } else {
+            println!("Unknown unit.");
+        }
     }
 }
